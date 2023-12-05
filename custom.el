7b772dbe30a94f641938364364f1cd2f513c1934
Reinout van Rees <reinout@vanrees.org>
2023-12-05 19:31:34 +0100

    local variables

--- custom.el
+++ custom.el
@@ -7,6 +7,9 @@
  '(inhibit-startup-screen t)
  '(kill-whole-line t)
  '(package-selected-packages '(modus-themes magit exec-path-from-shell better-defaults))
+ '(safe-local-variable-values
+   '((projectile-project-test-cmd . "docker compose run --rm web make test")
+     (projectile-project-compilation-cmd . "docker compose build")))
  '(split-height-threshold 200))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
