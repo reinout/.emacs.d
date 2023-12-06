9cbf135a0e14fc4870598f1a9e9cc616ac1f4441
Reinout van Rees <reinout@vanrees.org>
2023-12-06 23:31:37 +0100

    Storing recent customizations

--- custom.el
+++ custom.el
@@ -8,9 +8,15 @@
  '(kill-whole-line t)
  '(package-selected-packages '(modus-themes magit exec-path-from-shell better-defaults))
  '(safe-local-variable-values
-   '((projectile-project-test-cmd . "docker compose run --rm web make test")
+   '((projectile-project-test-cmd . "bin/pytest")
+     (projectile-project-compilation-cmd . "bin/pip install -r requirements.txt")
+     (lsp-pyright-python-venv-path . "~/zelf/advent-2023/")
+     (lsp-pyright-python-executable-cmd . "bin/python")
+     (python-pytest-executable . "bin/pytest")
+     (projectile-project-test-cmd . "docker compose run --rm web make test")
      (projectile-project-compilation-cmd . "docker compose build")))
- '(split-height-threshold 200))
+ '(split-height-threshold 200)
+ '(use-package-enable-imenu-support t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
