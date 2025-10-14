6b3d80395dbbd0f19ea66b2b13e2f5c12e27faef
Reinout van Rees <reinout@vanrees.org>
2025-10-14 14:06:19 +0200

    Closing succesful 'compile' windows after 2 seconds

--- init.el
+++ init.el
@@ -344,4 +344,24 @@
 
 (provide 'init.el)
 ;;; init.el ends here
+
 (put 'set-goal-column 'disabled nil)
+
+
+;; From https://stackoverflow.com/a/11059012/27401
+(defun bury-compile-buffer-if-successful (buffer string)
+ "Bury a compilation buffer if succeeded without warnings "
+ (when (and
+         (buffer-live-p buffer)
+         (string-match "compilation" (buffer-name buffer))
+         (string-match "finished" string)
+         (not
+          (with-current-buffer buffer
+            (goto-char (point-min))
+            (search-forward "warning" nil t))))
+    (run-with-timer 2 nil
+                    (lambda (buf)
+                      (bury-buffer buf)
+                      (switch-to-prev-buffer (get-buffer-window buf) 'kill))
+                    buffer)))
+(add-hook 'compilation-finish-functions 'bury-compile-buffer-if-successful)
